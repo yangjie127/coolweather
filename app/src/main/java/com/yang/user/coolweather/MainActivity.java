2c6fef4f81deaa173f0f72902478ceb87fe688c8
yangjie127 <12728347348@qq.com>
2017-03-02 11:03:52 +0800

    First commit.

---
+++ app/src/main/java/com/yang/user/coolweather/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yang.user.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    // //https://github.com/yangjie127/coolweather.git
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
